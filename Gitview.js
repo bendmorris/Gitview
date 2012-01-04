c994eaa877fbd625219cc51ec7824d84d4f14fc1
Paul Bouchon <bouchon@logicalcognition.com>
2012-01-04 09:52:37 -0500

    Code bath for sanity

--- Gitview.js
+++ Gitview.js
@@ -1,24 +1,24 @@
 var Gitview = function(args){
+	
+	// Builds each repo node
 	this.createRepoEntry = function(obj){
-		if(this.compact){
-			var container = dojo.create('div',{
-				style:"width:440px;border:1px solid #DDD;border-radius:4px;margin-bottom:10px;background:white"
-			},this.domNode);
-		}else{
-			var container = dojo.create('div',{
-				style:"width:440px;border:1px solid #DDD;border-radius:4px;margin-bottom:10px;background:white"
-			},this.domNode);
-		}
+		//1. repo container
+		var container = dojo.create('div',{
+			style:"width:440px;border:1px solid #DDD;border-radius:4px;margin-bottom:10px;background:white"
+		},this.domNode);
 		
+		//2. top (title, forks, watchers, etc.)
 		var top = dojo.create('div',{
 			style:"height:32px;line-height:38px;border-bottom:1px solid #DDD;"
 		},container);
 		
+		//3. smiley icon
 		var smiley = dojo.create('img',{
 			src:'https://a248.e.akamai.net/assets.github.com/images/icons/public.png',
 			style:'margin-left:6px'
 		},top);
 		
+		//4. title
 		var repoName = dojo.create('a',{
 			innerHTML: obj.name,
 			href:'https://github.com/'+this.user+'/'+obj.name,
@@ -28,51 +28,47 @@ var Gitview = function(args){
 		dojo.connect(repoName,'onmouseover',function(e){ dojo.style(e.target,'textDecoration','underline') });
 		dojo.connect(repoName,'onmouseout',function(e){ dojo.style(e.target,'textDecoration','none') });
 		
+		//5. container for watchers & forks
 		var stats = dojo.create('div',{
 			style:'display:inline-block;float:right;color:#666;font-size:11px;font-family:arial;font-weight:bold;'
 		},top);
 		
-		if(obj.language){
-			var lang = dojo.create('span',{
-				innerHTML:obj.language,
-				'style':'position:relative;top:-3px;'
-			},stats);
-		}
+		//6. language if there is one
+		if(obj.language)
+			var lang = dojo.create('span',{innerHTML:obj.language,'style':'position:relative;top:-3px;'},stats);
 		
+		//7. watchers
 		var watchers = dojo.create('a',{
 			innerHTML:'<img src="http://logicalcognition.com/images/repostat_watchers.png"/>&nbsp;<font color="#666;">'+obj.watchers+'</font>',
 			href:'https://github.com/'+this.user+'/'+obj.name+'/watchers',
 			style:'position:relative;top:-3px;margin-left:10px;text-decoration:none;'
 		},stats);
 		
+		//8. forks
 		var forks = dojo.create('a',{
 			innerHTML:'<img src="http://logicalcognition.com/images/repostat_forks.png"/>&nbsp;<font color="#666;">'+obj.forks+'</font>',
 			href:'https://github.com/'+this.user+'/'+obj.name+'/network',
 			style:'position:relative;top:-3px;margin-left:10px;text-decoration:none;margin-right:15px'
 		},stats);
 		
-		if(this.compact){
-			var bottom = dojo.create('div',{
-				style:'height:30px;border-bottom-right-radius:3px;border-bottom-left-radius:3px;padding-bottom:5px;padding-top:5px'
-			},container);	
-		}else{
-			var bottom = dojo.create('div',{
-				style:'border-bottom-right-radius:3px;border-bottom-left-radius:3px;padding-bottom:5px;padding-top:5px'
-			},container);
-		}
+		//9. bottom (participation graph, last updated)
+		var bottom = dojo.create('div',{
+			style:'border-bottom-right-radius:3px;border-bottom-left-radius:3px;padding-bottom:5px;padding-top:5px'
+		},container);
+		if(this.compact)
+			dojo.style(bottom,'height','30px');
 		if(dojo.isWebKit)
 			dojo.style(bottom,'backgroundImage',"-webkit-gradient(linear, 0% 0%, 0% 100%, from(#FAFAFA), to(#EFEFEF))");
 		else if(dojo.isFF)
 			dojo.style(bottom,'background','-moz-linear-gradient(center top , #FAFAFA, #EFEFEF) repeat scroll 0 0 transparent');
 		
+		//10. Slice & build repo description
 		var d = obj.description;
 		if(d.length > 100)
 			d = d.slice(0,97)+'...';
-		var description = dojo.create('div',{
-			innerHTML:d,
-			style:'font:12px arial;margin-left:9px;'
-		},bottom);
+		var description = dojo.create('div',{innerHTML:d,style:'font:12px arial;margin-left:9px;'},bottom);
 		
+		//11. Participation graph & last updated
 		if(!this.compact){
 			var updated = dojo.create('div',{
 				innerHTML:'Last updated '+this.fixUpdateDate(obj.updated_at),
@@ -86,44 +82,33 @@ var Gitview = function(args){
 		}	
 	};
 	
+	// Changes regular formatted date into '1 day ago', '9 hours ago', etc.
 	this.fixUpdateDate = function(date){
+		// TODO: currently I just return the date as-is, I need to implement this
 		return date;
 	};
 	
+	// Dynamically load JS script with callback
 	this.loadScript = function(sScriptSrc,callbackfunction) {
-		//gets document head element
 		var oHead = document.getElementsByTagName('head')[0];
-		if(oHead){
-			//creates a new script tag		
+		if(oHead){	
 			var oScript = document.createElement('script');
-
-			//adds src and type attribute to script tag
 			oScript.setAttribute('src',sScriptSrc);
 			oScript.setAttribute('type','text/javascript');
-
-			//calling a function after the js is loaded (IE)
-			var loadFunction = function()
-				{
-					if (this.readyState == 'complete' || this.readyState == 'loaded')
-					{
-						callbackfunction(); 
-					}
-				};
+			var loadFunction = function(){
+				if (this.readyState == 'complete' || this.readyState == 'loaded')
+					callbackfunction(); 
+			};
 			oScript.onreadystatechange = loadFunction;
-
-			//calling a function after the js is loaded (Firefox)
-			oScript.onload = callbackfunction;
-
-			//append the script tag to document head element		
+			oScript.onload = callbackfunction;	
 			oHead.appendChild(oScript);
 		}
 	};
 	
+	// Bind, for browsers not supporting it by default
 	this.bind = function (oThis) {
-		if (typeof this !== "function") {
-		  // closest thing possible to the ECMAScript 5 internal IsCallable function
+		if (typeof this !== "function")
 		  throw new TypeError("Function.prototype.bind - what is trying to be bound is not callable");
-		}
 
 		var aArgs = Array.prototype.slice.call(arguments, 1), 
 		    fToBind = this, 
@@ -134,33 +119,22 @@ var Gitview = function(args){
 		                             : oThis || window,
 		                           aArgs.concat(Array.prototype.slice.call(arguments)));
 		    };
-
 		fNOP.prototype = this.prototype;
 		fBound.prototype = new fNOP();
-
 		return fBound;
 	};
-
-	//Initiatialization
-	if(!args || !args.user || !args.domNode){
-		throw new Error('Gitview: missing user and/or domNode arg');
-	}else{
-		this.domNode = args.domNode;
-		this.user = args.user;
-		this.repos = [];
-		this.compact = args.compact==true ? true : false;
-		
-		if (!Function.prototype.bind)
-		  Function.prototype.bind = this.bind;
-		
-		var bootstrap = function(){
-			if(!dojo)
-				this.loadScript('http://ajax.googleapis.com/ajax/libs/dojo/1.7.1/dojo/dojo.js',kickStart.bind(this));
-			else
-				kickStart.bind(this)();
-		};
-		
-		var kickStart = function(){
+	
+	// Get required scripts loaded
+	this.bootstrap = function(){
+		if(!window.dojo)
+			this.loadScript('http://ajax.googleapis.com/ajax/libs/dojo/1.7.1/dojo/dojo.js',this.kickStart.bind(this));
+		else
+			this.kickStart.bind(this)();
+	};
+	
+	// Kick things off once Gitgraph is loaded
+	this.kickStart = function(){
+		dojo.ready(this,function(){
 			dojo.xhrGet({
 				url: 'https://api.github.com/users/'+this.user+'/repos',
 				handleAs: 'json',
@@ -172,8 +146,21 @@ var Gitview = function(args){
 			});
 			for(var i in this.repos)
 				this.createRepoEntry(this.repos[i]);
-		};
+		});
+	};
+	
+	//Initiatialization
+	if(!args || !args.user || !args.domNode){
+		throw new Error('Gitview: missing user and/or domNode arg');
+	}else{
+		this.domNode 	= args.domNode;
+		this.user 		= args.user;
+		this.repos 		= [];
+		this.compact 	= args.compact==true ? true : false;
 		
-		this.loadScript('http://logicalcognition.com/Projects/Gitgraph/Gitgraph.js',bootstrap.bind(this));	
+		if (!Function.prototype.bind)
+		  Function.prototype.bind = this.bind;
+		
+		this.loadScript('http://logicalcognition.com/Projects/Gitgraph/Gitgraph.js',this.bootstrap.bind(this));	
 	}
 };

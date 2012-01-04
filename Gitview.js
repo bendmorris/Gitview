ec78b76e1f3e56dfe7a04db84550f9c21c72cce0
Paul Bouchon <bouchon@logicalcognition.com>
2012-01-04 09:26:52 -0500

    Fix for multiple-define issue

--- Gitview.js
+++ Gitview.js
@@ -154,7 +154,10 @@ var Gitview = function(args){
 		  Function.prototype.bind = this.bind;
 		
 		var bootstrap = function(){
-			this.loadScript('http://ajax.googleapis.com/ajax/libs/dojo/1.7.1/dojo/dojo.js',kickStart.bind(this));
+			if(!dojo)
+				this.loadScript('http://ajax.googleapis.com/ajax/libs/dojo/1.7.1/dojo/dojo.js',kickStart.bind(this));
+			else
+				kickStart.bind(this)();
 		};
 		
 		var kickStart = function(){

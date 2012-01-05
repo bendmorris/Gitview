bba976936e2907c0a0179f855ecf934491b55ec9
Paul Bouchon <bouchon@logicalcognition.com>
2012-01-05 14:22:13 -0500

    Force text-align left

--- Gitview.js
+++ Gitview.js
@@ -268,7 +268,7 @@ var Gitview = function(args){
 				style:'height:100%;overflow-y:auto;width:'+this.w+';'
 			},outer);
 			this.domNode = inner;
-			dojo.style(this.domNode,'text-align','left');
+			dojo.style(this.domNode,'textAlign','left');
 		}
 		
 		this.loadScript('http://logicalcognition.com/Projects/Gitgraph/Gitgraph.js',this.bootstrap.bind(this));	
